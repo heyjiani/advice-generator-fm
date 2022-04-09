2f0fd2500082fdb2095ebaf20a0214b5de7e6f0c
Jiani Cao <jianicakes@gmail.com>
2022-04-08 20:27:26 -0700

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
+      <Card />
     </div>
   );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,5 @@
+export default function Card(props) {
+  return(
+    <h1>Hello, this is Card</h1>
+  )
+}
