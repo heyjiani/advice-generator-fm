a379030735561955d0d5894d228163c6f5b8095c
Jiani Cao <jianicakes@gmail.com>
2022-04-09 00:01:19 -0700

    add basic stylings to card

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,9 +2,10 @@ export default function Card(props) {
   const { id, advice } = props;
 
   return(
-    <article>
-      <h1>Advice #{id}</h1>
-      <p>{advice}</p>
+    <article className="advice">
+      <h1 className="advice__header">Advice #{id}</h1>
+      <p className="advice__text">"{advice}"</p>
+      <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
     </article>
   )
 }
