4c766b6df7f4b81d9dbb1023bb95b0010254e933
Jiani Cao <jianicakes@gmail.com>
2022-04-09 01:51:56 -0700

    implement onclick refresh

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import Card from './Card';
 function App() {
   const [adviceSlip, setAdviceSlip] = useState({});
 
-  useEffect(() => {
+  const fetchData = () => {
     axios
-    .get('https://api.adviceslip.com/advice')
-    .then(res => setAdviceSlip(res.data.slip))
-    .catch(err => console.log(err))
+      .get('https://api.adviceslip.com/advice')
+      .then(res => setAdviceSlip(res.data.slip))
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   return (
     <main className="layout">
-      <Card {...adviceSlip}/>
+      <Card {...adviceSlip} handleClick={fetchData} />
     </main>
   );
 }
